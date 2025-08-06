6f3e4e482254a953ce0b7c891ce05b088d75c410
David <david@webcodr.io>
2025-08-06 22:14:15 +0200

    refactor: fix minor issues and add tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -51,3 +51,123 @@ fn fails_on_too_many_attempts_custom() {
             "Could not connect to server Hello World after 5 attempts",
         ));
 }
+
+#[test]
+fn fails_on_empty_server_list() {
+    let mut command = Command::cargo_bin("server-runner").unwrap();
+
+    command
+        .arg("-c")
+        .arg("empty_servers.yaml")
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains(
+            "Configuration must include at least one server",
+        ));
+}
+
+#[test]
+fn fails_on_timeout_with_custom_timeout() {
+    let mut command = Command::cargo_bin("server-runner").unwrap();
+
+    command
+        .arg("-c")
+        .arg("timeout_test.yaml")
+        .arg("-a")
+        .arg("2")
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains(
+            "Could not connect to server Timeout Test Server after 2 attempts",
+        ));
+}
+
+#[test]
+fn fails_on_empty_command() {
+    let mut command = Command::cargo_bin("server-runner").unwrap();
+
+    command
+        .arg("-c")
+        .arg("empty_command.yaml")
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains(
+            "Configuration must include a command to run",
+        ));
+}
+
+#[test]
+fn fails_on_invalid_yaml() {
+    let mut command = Command::cargo_bin("server-runner").unwrap();
+
+    command
+        .arg("-c")
+        .arg("invalid_yaml.yaml")
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains(
+            "Could not find config file invalid_yaml.yaml",
+        ));
+}
+
+#[test]
+fn fails_on_missing_required_fields() {
+    let mut command = Command::cargo_bin("server-runner").unwrap();
+
+    command
+        .arg("-c")
+        .arg("missing_fields.yaml")
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains(
+            "Could not parse config file missing_fields.yaml",
+        ));
+}
+
+#[test]
+fn fails_on_multiple_unreachable_servers() {
+    let mut command = Command::cargo_bin("server-runner").unwrap();
+
+    command
+        .arg("-c")
+        .arg("multiple_servers.yaml")
+        .arg("-a")
+        .arg("2")
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains(
+            "Could not connect to server",
+        ));
+}
+
+#[test]
+fn fails_on_zero_timeout() {
+    let mut command = Command::cargo_bin("server-runner").unwrap();
+
+    command
+        .arg("-c")
+        .arg("zero_timeout.yaml")
+        .arg("-a")
+        .arg("1")
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains(
+            "Could not connect to server Zero Timeout Server after 1 attempts",
+        ));
+}
+
+#[test]
+fn fails_on_one_attempt() {
+    let mut command = Command::cargo_bin("server-runner").unwrap();
+
+    command
+        .arg("-c")
+        .arg("timeout_test.yaml")
+        .arg("-a")
+        .arg("1")
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains(
+            "Could not connect to server Timeout Test Server after 1 attempts",
+        ));
+}
